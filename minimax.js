b69cc19187e39e0c08024114d361cab3ca2ff377
Your Name <you@example.com>
2020-07-20 17:04:11 +0530

    changed hover

--- minimax.js
+++ minimax.js
@@ -10,328 +10,332 @@ var maxDepth = 10;
 var truedepth = maxDepth;
 var depth;
 const WinningCombs = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
 
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
 
-  [0, 4, 8],
-  [2, 4, 6],
+    [0, 4, 8],
+    [2, 4, 6],
 ];
 
 const cells = $(".cell");
 
 function SetLevelOne() {
-  maxDepth = 1;
-  truedepth = maxDepth;
+    maxDepth = 1;
+    truedepth = maxDepth;
 }
 
 function SetLevelTwo() {
-  maxDepth = 2;
-  truedepth = maxDepth;
+    maxDepth = 2;
+    truedepth = maxDepth;
 }
 
 function SetLevelThree() {
-  maxDepth = 3;
-  truedepth = maxDepth;
+    maxDepth = 3;
+    truedepth = maxDepth;
 }
 
 function SetLevelInf() {
-  maxDepth = 9;
-  truedepth = maxDepth;
+    maxDepth = 9;
+    truedepth = maxDepth;
 }
-$(".button").on("click", function () {
-  $(".btn-group").addClass("noHover");
-  $(this).css("color", "#723267");
+$(".button").on("click", function() {
+    $(".btn-group").addClass("noHover");
+    $(this).css("color", "#723267");
 });
 
-function startGame() {
-  maxDepth = 10;
-  truedepth = 10;
-  $(".button").css("color", "white");
-  $(".sug").css("color", "#723267");
-  $("table").animate({
-    opacity: "1",
-  });
-  if (MultiPlayer == true) {
-    $(".sug").removeClass("disabled");
-    $(".sug").removeClass("noHover");
-  }
-  if (MultiPlayer == false) {
-    $(".btn-group").removeClass("disabled");
-    $(".btn-group").removeClass("noHover");
-    $(".sug").removeClass("disabled");
-    $(".sug").removeClass("noHover");
-  }
-  PresentTurn = player;
-  PresentPlayTag = $("#PresentTurn");
-  $("#f1").text(PresentTurn);
-  $("#f2").text(PresentTurn == "X" ? "O" : "X");
+$(".dropdown").click(function({
+    $(this).addClass('clicked')
 
-  cells.on("click", PresentTurnClick);
-  EndOfGame = 0;
-  $(".end-game").value = "none";
-  boardGame = Array.from(Array(9).keys());
-  ClearTicTacToe();
+}));
+
+function startGame() {
+    maxDepth = 10;
+    truedepth = 10;
+    $(".button").css("color", "white");
+    $(".sug").css("color", "#723267");
+    $("table").animate({
+        opacity: "1",
+    });
+    if (MultiPlayer == true) {
+        $(".sug").removeClass("disabled");
+        $(".sug").removeClass("noHover");
+    }
+    if (MultiPlayer == false) {
+        $(".btn-group").removeClass("disabled");
+        $(".btn-group").removeClass("noHover");
+        $(".sug").removeClass("disabled");
+        $(".sug").removeClass("noHover");
+    }
+    PresentTurn = player;
+    PresentPlayTag = $("#PresentTurn");
+    $("#f1").text(PresentTurn);
+    $("#f2").text(PresentTurn == "X" ? "O" : "X");
+
+    cells.on("click", PresentTurnClick);
+    EndOfGame = 0;
+    $(".end-game").value = "none";
+    boardGame = Array.from(Array(9).keys());
+    ClearTicTacToe();
 }
 
 function endGame() {
-  console.log(c);
-  $("table").animate({
-    opacity: "0.4",
-  });
-  EndOfGame = 1;
-  ClearTicTacToe();
-  PresentPlayTag.text("- - - - - - - -");
-  cells.off("click");
+    console.log(c);
+    $("table").animate({
+        opacity: "0.4",
+    });
+    EndOfGame = 1;
+    ClearTicTacToe();
+    PresentPlayTag.text("- - - - - - - -");
+    cells.off("click");
 
-  $(".btn-group").addClass("disabled");
-  $(".sug").addClass("disabled");
-  maxDepth = 10;
-  $(".button").css("color", "white");
+    $(".btn-group").addClass("disabled");
+    $(".sug").addClass("disabled");
+    maxDepth = 10;
+    $(".button").css("color", "white");
 }
 
 function PresentTurnClick(square) {
-  if (typeof boardGame[square.target.id] == "number") {
-    turn(square.target.id, PresentTurn);
-    if (!CheckForWin(boardGame, player) && !CheckForTie()) {
-      if (MultiPlayer === false) {
-        cells.off("click");
-        setTimeout(function () {
-          turn(bestChoice(false), player_2);
-          cells.on("click", PresentTurnClick);
-          $(".sug").on("click", function () {
-            suggestions(bestChoice(true), player);
-          });
-        }, 700);
-      } else if (MultiPlayer === true) {
-        $(".sug").on("click", function () {
-          suggestions(bestChoice(true), player);
-        });
-      }
+    if (typeof boardGame[square.target.id] == "number") {
+        turn(square.target.id, PresentTurn);
+        if (!CheckForWin(boardGame, player) && !CheckForTie()) {
+            if (MultiPlayer === false) {
+                cells.off("click");
+                setTimeout(function() {
+                    turn(bestChoice(false), player_2);
+                    cells.on("click", PresentTurnClick);
+                    $(".sug").on("click", function() {
+                        suggestions(bestChoice(true), player);
+                    });
+                }, 700);
+            } else if (MultiPlayer === true) {
+                $(".sug").on("click", function() {
+                    suggestions(bestChoice(true), player);
+                });
+            }
+        }
     }
-  }
 }
 
 function turn(boxId, player) {
-  currentPlayer = player == "X" ? "O" : "X";
-  if (MultiPlayer == false) {
-    if (currentPlayer != player_2)
-      PresentPlayTag.text("You Play Next: " + currentPlayer);
-    else PresentPlayTag.text("AI's turn " + currentPlayer);
-  } else PresentPlayTag.text("You Play Next: " + currentPlayer);
-  boardGame[boxId] = player;
-  $("#" + boxId).text(player);
-  let gameFinished = CheckForWin(boardGame, player);
-  if (gameFinished) {
-    gameOver(gameFinished);
-  }
+    currentPlayer = player == "X" ? "O" : "X";
+    if (MultiPlayer == false) {
+        if (currentPlayer != player_2)
+            PresentPlayTag.text("You Play Next: " + currentPlayer);
+        else PresentPlayTag.text("AI's turn " + currentPlayer);
+    } else PresentPlayTag.text("You Play Next: " + currentPlayer);
+    boardGame[boxId] = player;
+    $("#" + boxId).text(player);
+    let gameFinished = CheckForWin(boardGame, player);
+    if (gameFinished) {
+        gameOver(gameFinished);
+    }
 
-  PresentTurn = PresentTurn == "X" ? "O" : "X";
+    PresentTurn = PresentTurn == "X" ? "O" : "X";
 }
 
 function MinMax(PresentGameBoard, currentPlayer, depth, flag) {
-  c++;
-  if (flag == true) maxDepth = 10;
-  if (flag == false) maxDepth = truedepth;
-  var availableMoves = AvailableMoves();
+    c++;
+    if (flag == true) maxDepth = 10;
+    if (flag == false) maxDepth = truedepth;
+    var availableMoves = AvailableMoves();
 
-  if (CheckForWin(PresentGameBoard, player)) {
-    return {
-      score: -100 + depth,
-    };
-  } else if (CheckForWin(PresentGameBoard, player_2)) {
-    return {
-      score: 100 - depth,
-    };
-  } else if (availableMoves.length === 0 || depth == maxDepth) {
-    return {
-      score: 0,
-    };
-  }
+    if (CheckForWin(PresentGameBoard, player)) {
+        return {
+            score: -100 + depth,
+        };
+    } else if (CheckForWin(PresentGameBoard, player_2)) {
+        return {
+            score: 100 - depth,
+        };
+    } else if (availableMoves.length === 0 || depth == maxDepth) {
+        return {
+            score: 0,
+        };
+    }
 
-  var moves = [];
-  for (var i = 0; i < availableMoves.length; i++) {
-    var move = {};
-    move.index = PresentGameBoard[availableMoves[i]];
-    PresentGameBoard[availableMoves[i]] = currentPlayer;
+    var moves = [];
+    for (var i = 0; i < availableMoves.length; i++) {
+        var move = {};
+        move.index = PresentGameBoard[availableMoves[i]];
+        PresentGameBoard[availableMoves[i]] = currentPlayer;
 
-    if (currentPlayer == player_2) {
-      var maxScoreIndex = MinMax(PresentGameBoard, player, depth + 1, flag);
-      move.score = maxScoreIndex.score;
+        if (currentPlayer == player_2) {
+            var maxScoreIndex = MinMax(PresentGameBoard, player, depth + 1, flag);
+            move.score = maxScoreIndex.score;
+        } else {
+            var maxScoreIndex = MinMax(PresentGameBoard, player_2, depth + 1, flag);
+            move.score = maxScoreIndex.score;
+        }
+
+        PresentGameBoard[availableMoves[i]] = move.index;
+
+        moves.push(move);
+    }
+
+    var bestChoice;
+    if (currentPlayer === player_2) {
+        var bestScore = -10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestChoice = i;
+            }
+        }
     } else {
-      var maxScoreIndex = MinMax(PresentGameBoard, player_2, depth + 1, flag);
-      move.score = maxScoreIndex.score;
+        var bestScore = 10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestChoice = i;
+            }
+        }
     }
 
-    PresentGameBoard[availableMoves[i]] = move.index;
-
-    moves.push(move);
-  }
-
-  var bestChoice;
-  if (currentPlayer === player_2) {
-    var bestScore = -10000;
-    for (var i = 0; i < moves.length; i++) {
-      if (moves[i].score > bestScore) {
-        bestScore = moves[i].score;
-        bestChoice = i;
-      }
-    }
-  } else {
-    var bestScore = 10000;
-    for (var i = 0; i < moves.length; i++) {
-      if (moves[i].score < bestScore) {
-        bestScore = moves[i].score;
-        bestChoice = i;
-      }
-    }
-  }
-
-  return moves[bestChoice];
+    return moves[bestChoice];
 }
 
 function CheckForWin(board, player) {
-  let plays = board.reduce(
-    (acc, elem, index) => (elem === player ? acc.concat(index) : acc),
-    []
-  );
-  let gameFinished = null;
-  for (let [index, win] of WinningCombs.entries()) {
-    if (win.every((elem) => plays.indexOf(elem) > -1)) {
-      gameFinished = {
-        index: index,
-        player: player,
-      };
-      break;
+    let plays = board.reduce(
+        (acc, elem, index) => (elem === player ? acc.concat(index) : acc), []
+    );
+    let gameFinished = null;
+    for (let [index, win] of WinningCombs.entries()) {
+        if (win.every((elem) => plays.indexOf(elem) > -1)) {
+            gameFinished = {
+                index: index,
+                player: player,
+            };
+            break;
+        }
     }
-  }
-  return gameFinished;
+    return gameFinished;
 }
 
 function gameOver(gameFinished) {
-  EndOfGame = 1;
-  maxDepth = 10;
-  cells.off("click");
-  cells.animate({
-    opacity: "0.4",
-  });
-  for (let index of WinningCombs[gameFinished.index]) {
-    setTimeout(function () {
-      $("#" + index).css("background-color", "green");
-      $("#" + index).animate({
-        opacity: "1",
-      });
-      $("#PresentTurn").text(gameFinished.player + " WINS");
-    }, 500);
-  }
+    EndOfGame = 1;
+    maxDepth = 10;
+    cells.off("click");
+    cells.animate({
+        opacity: "0.4",
+    });
+    for (let index of WinningCombs[gameFinished.index]) {
+        setTimeout(function() {
+            $("#" + index).css("background-color", "green");
+            $("#" + index).animate({
+                opacity: "1",
+            });
+            $("#PresentTurn").text(gameFinished.player + " WINS");
+        }, 500);
+    }
 }
 
 function AvailableMoves() {
-  return boardGame.filter((s) => typeof s == "number");
+    return boardGame.filter((s) => typeof s == "number");
 }
 
 function CheckForTie() {
-  if (AvailableMoves().length === 0 && CheckForWin != true) {
-    EndOfGame = 1;
-    cells.off("click");
-    cells.animate({
-      opacity: "0.4",
-    });
-    PresentPlayTag.text("TIE GAME!");
+    if (AvailableMoves().length === 0 && CheckForWin != true) {
+        EndOfGame = 1;
+        cells.off("click");
+        cells.animate({
+            opacity: "0.4",
+        });
+        PresentPlayTag.text("TIE GAME!");
 
-    return true;
-  }
-  return false;
+        return true;
+    }
+    return false;
 }
 
 function bestChoice(flag) {
-  return MinMax(boardGame, player_2, 0, flag).index;
+    return MinMax(boardGame, player_2, 0, flag).index;
 }
 
 function ClearTicTacToe() {
-  for (var i = 0; i < cells.length; i++) {
-    $("#" + i).text("");
-  }
+    for (var i = 0; i < cells.length; i++) {
+        $("#" + i).text("");
+    }
 
-  for (var i = 0; i < cells.length; i++) {
-    cells[i].value = "";
-    cells[i].style.removeProperty("background-color");
-    cells[i].style.removeProperty("opacity");
-    cells.on("click", PresentTurnClick);
-  }
-  PresentPlayTag.text("You Play Next: " + player);
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].value = "";
+        cells[i].style.removeProperty("background-color");
+        cells[i].style.removeProperty("opacity");
+        cells.on("click", PresentTurnClick);
+    }
+    PresentPlayTag.text("You Play Next: " + player);
 }
 
 function ReverseIt() {
-  if (EndOfGame === 1 && MultiPlayer == true) {
-    PresentTurn = PresentTurn == player ? player_2 : player;
-    player = player == "X" ? "O" : "X";
-    player_2 = player_2 == "X" ? "O" : "X";
-    PresentFigureCh = $("#f1").text();
-    $("#f1").animate({
-      opacity: "0",
-    });
-    $("#f2").animate({
-      opacity: "0",
-    });
-    setTimeout(function () {
-      $("#f1").text((PresentFigureCh = PresentFigureCh == "X" ? "O" : "X"));
-      $("#f2").text((PresentFigureCh = PresentFigureCh == "X" ? "O" : "X"));
-    }, 500);
-    $("#f1").animate({
-      opacity: "1",
-    });
-    $("#f2").animate({
-      opacity: "1",
-    });
-  }
+    if (EndOfGame === 1 && MultiPlayer == true) {
+        PresentTurn = PresentTurn == player ? player_2 : player;
+        player = player == "X" ? "O" : "X";
+        player_2 = player_2 == "X" ? "O" : "X";
+        PresentFigureCh = $("#f1").text();
+        $("#f1").animate({
+            opacity: "0",
+        });
+        $("#f2").animate({
+            opacity: "0",
+        });
+        setTimeout(function() {
+            $("#f1").text((PresentFigureCh = PresentFigureCh == "X" ? "O" : "X"));
+            $("#f2").text((PresentFigureCh = PresentFigureCh == "X" ? "O" : "X"));
+        }, 500);
+        $("#f1").animate({
+            opacity: "1",
+        });
+        $("#f2").animate({
+            opacity: "1",
+        });
+    }
 }
 
 function activeMultiPlayer() {
-  if (EndOfGame === 1) {
-    if (MultiPlayer) {
-      $("#p2").animate({
-        opacity: "0.3",
-      });
-    } else {
-      $("#p2").animate({
-        opacity: "1",
-      });
+    if (EndOfGame === 1) {
+        if (MultiPlayer) {
+            $("#p2").animate({
+                opacity: "0.3",
+            });
+        } else {
+            $("#p2").animate({
+                opacity: "1",
+            });
+        }
+        MultiPlayer = MultiPlayer == true ? false : true;
     }
-    MultiPlayer = MultiPlayer == true ? false : true;
-  }
 }
 
 function suggestions(boxId, player) {
-  if (AvailableMoves.length != 9) {
-    setTimeout(function () {
-      $("#" + boxId).animate({
-        opacity: "0.6",
-      });
-    }, 3);
-    setTimeout(function () {
-      $("#" + boxId).animate({
-        opacity: "1",
-      });
-    }, 7);
+    if (AvailableMoves.length != 9) {
+        setTimeout(function() {
+            $("#" + boxId).animate({
+                opacity: "0.6",
+            });
+        }, 3);
+        setTimeout(function() {
+            $("#" + boxId).animate({
+                opacity: "1",
+            });
+        }, 7);
 
-    $(".sug").removeClass("disabled");
-    $(".sug").removeClass("noHover");
-  }
+        $(".sug").removeClass("disabled");
+        $(".sug").removeClass("noHover");
+    }
 }
 
 
 
-  $(".feature").hover(function(){
+$(".feature").hover(function() {
     $(".feat").fadeToggle("slow");
-  });
-  $(".howtoplay").hover(function(){
+});
+$(".howtoplay").hover(function() {
     $(".htplay").fadeToggle("slow");
-  });
-  $(".contactus").hover(function(){
+});
+$(".contactus").hover(function() {
     $(".contactuss").fadeToggle("slow");
-  });
+});
